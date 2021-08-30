a63f940717ce7c7fa821aedefe746ef19c15a69e
Luay Kalboni <luay221221@cs.colman.ac.il>
2021-08-30 21:28:01 +0300

    changing settings

--- ShopApp/Models/User.cs
+++ ShopApp/Models/User.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace GameStore_IL.Models
+namespace Store_L.Models
 {
     public class User
     {
